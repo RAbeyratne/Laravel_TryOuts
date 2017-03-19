22bfc9a489d6aa45300c4989c17ad7e06ea97ad5
Ramitha Abeyratne <ramithaabeyratne@rocketmail.com>
2017-03-19 20:07:36 +0530

    Blade and routes basic tryout

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/abc', function () {
+    $tArray = ["Uno", "Dos", "Tres"];
+    return view('testpage') -> with(['tArray' => $tArray]);
+});
